d4b98ed258622e184b3434cf1f455704f28a708a
Niciolla <39137998+Niciolla@users.noreply.github.com>
2023-03-27 16:27:43 +0200

    speech directory added, requirements.txt file has been generated.

--- analysis/Frame.py
+++ analysis/Frame.py
@@ -2,6 +2,12 @@ class Frame:
     def __init(self, id, question, **kwargs):
         self.id = id
         self.question = question
+        self.slots = dict(intent=None, domain=None) | kwargs
+
+        self.slots["intent"] = kwargs["intent"]
+
+    def modify_slot(self, slot: str):
+        self.slots[slot] = "Nuovo Valore"
 
 
 
--- generation/NaturalLanguageGenerator.py
+++ generation/NaturalLanguageGenerator.py
@@ -1,5 +1,6 @@
 import simplenlg
 
+
 class NaturalLanguageGenerator:
     def __init__(self):
         pass
