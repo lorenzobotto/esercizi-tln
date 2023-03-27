b220735c20ef11ef49368bda7858318f01b61f06
Niciolla <39137998+Niciolla@users.noreply.github.com>
2023-03-27 15:31:24 +0200

    created directory structure.

---
+++ analysis/Frame.py
@@ -0,0 +1,8 @@
+class Frame:
+    def __init(self, id, question, **kwargs):
+        self.id = id
+        self.question = question
+
+
+
+
---
+++ generation/NaturalLanguageGenerator.py
@@ -0,0 +1,5 @@
+import simplenlg
+
+class NaturalLanguageGenerator:
+    def __init__(self):
+        pass
