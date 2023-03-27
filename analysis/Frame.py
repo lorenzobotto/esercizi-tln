4a7d83c77d0770d1f285438821e00a31e751d5d6
Gabriele Naretto <narettogabriele@gmail.com>
2023-03-27 16:28:24 +0200

    parcial costruction frame class

--- analysis/Frame.py
+++ analysis/Frame.py
@@ -1,7 +1,14 @@
 class Frame:
-    def __init(self, id, question, **kwargs):
-        self.id = id
+    def __init__(self,question,domain,intent,**kwargs):
         self.question = question
+        self.slot = dict(domain=domain,intent=intent) | kwargs
+
+    def modify_slot(self,edit):#edit = dict
+        self.slot|edit #modify the value of the slot
+
+
+
+
 
 
 
