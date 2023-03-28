c3802861691a2f0b794492f2da4e7d9a18f262c8
Gabriele Naretto <narettogabriele@gmail.com>
2023-03-28 15:40:27 +0200

    draft frame resolve method and regex class

--- analysis/Frame.py
+++ analysis/Frame.py
@@ -1,11 +1,20 @@
+from regex import regex_q1
+
+
 class Frame:
-    def __init__(self,question,domain,intent,**kwargs):
+    def __init__(self,question,domain,intent,regex_set: set = None, **kwargs):
         self.question = question
         self.slot = dict(domain=domain,intent=intent) | kwargs
+        self.regex = regex_set
+
+
+        
 
     def modify_slot(self,edit):#edit = dict
         self.slot|edit #modify the value of the slot
-
+    
+    def resolve(self,string):
+        pass
 
 
 
--- analysis/Regex.py
+++ analysis/Regex.py
@@ -7,17 +7,22 @@ test_string_error_word = "coruskant"#funziona per pattern 3
 test_string_error_word2 = "corusant"#funziona per pattern 2 e 3
 
 #pattern 
-pattern = "coruscant" #funziona in caso di frase corretta
-pattern2 = "corusant" #funziona in caso di mancanza di una lettera (errore molto comune)
-pattern3 = "cor[a-z]{1,3}ant" #funziona in caso di uno o più errori multipli al centro della
-patter4 = "(located|situated) on cor[a-z]{1,3}ant"
+pattern = "coruscant" #funziona se ce la parola
+pattern2 = "cor[a-z]{1,3}ant" #funziona se ce la parola ma con qualche errore
+#in caso di frasi più corrette
+pattern3 = "is (located|situated) on cor[a-z]{1,3}ant" 
+pattern4 = "cor[a-z]{1,3}ant is the base" 
+pattern5 = "the base is on cor[a-z]{1,3}ant"
 
 
 """
+frasi negative
+----------------------------------------------da risolvere 
+The headquarters of the Jedi Order is located on a planet other than coruscant.
+
 ----------------------------------------------risolti
 the headquarters is not on coruscant.
 it is not coruscant
-is not coruscant
 isn't coruscant
 The headquarters of the Jedi Order is not situated on coruscant.
 The headquarters of the Jedi Order is not located on any planet.
@@ -25,25 +30,28 @@ The headquarters of the Jedi Order is not located on any planet.
 ----------------------------------------------si ignora e si dice che hai sbagliato
 The Jedi Order does not exist in the Star Wars universe.
 The Jedi Order does not have a headquarters. 
-
-The headquarters of the Jedi Order is located on a planet other than coruscant.
 The planet Coruscant does not exist in the Star Wars universe.
 
 ---------------------------------------------frasi stronze
 The headquarters of the Sith Order is located on Coruscant instead of the Jedi Order. 
 """
 #frasi con negazioni
-negativephrase = ""
+negativephrase = "isn't coruscant"
 
 #pattern per trocare le negazioni
 patternNegative="(is not|isn't) on cor[a-z]{1,3}ant"
 patternNegative2="(is not|isn't) (located|situated) on cor[a-z]{1,3}ant"
 patternNegative3 = "(is not|isn't) (located|situated) on any planet"
+patternNegative4 = "(is not|isn't) cor[a-z]{1,3}ant"
 
-match = re.search(patternNegative3, negativephrase7) 
+match = re.search(patternNegative4, negativephrase) 
 
 if match:
   print(match.group())
   print("TRUE")
 else:
-  print("pattern not found")
+  print("pattern not found")ù
+
+
+
+regex_q1 = set(patternNegative, patternNegative2, patternNegative3)
