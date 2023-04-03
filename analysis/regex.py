fa4425662cf83ff079fb1d9f73c054e4150c0d61
Gabriele Naretto <narettogabriele@gmail.com>
2023-04-03 16:30:07 +0200

    more question, and all question have regex

--- analysis/regex.py
+++ analysis/regex.py
@@ -92,10 +92,10 @@ q1PosPattern4 = "cor[a-z]{1,3}ant is the base"
 q1PosPattern5 = "the base is on cor[a-z]{1,3}ant"
 
 # pattern per frasi con negazione
-q1NegPattern1 = "(is not|isn't) on cor[a-z]{1,3}ant"
-q1NegPattern2 = "(is not|isn't) (located|situated) on cor[a-z]{1,3}ant"
-q1NegPattern3 = "(is not|isn't) (located|situated) on (any planet|a planet)"
-q1NegPattern4 = "(is not|isn't) cor[a-z]{1,3}ant"
+q1NegPattern1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) on cor[a-z]{1,3}ant"
+q1NegPattern2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (located|situated) on cor[a-z]{1,3}ant"
+q1NegPattern3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (located|situated) on (any planet|a planet)"
+q1NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) cor[a-z]{1,3}ant"
 q1NegPattern5 = "(located|situated) on ((?!cor[a-z]{1,3}ant).)*$"  # dico che il centro di comando e situato su un altro pianeta
 q1NegPattern6 = "^((?!cor[a-z]{1,3}ant).)*$"  # dico il nome di un altra citta
 
@@ -162,23 +162,23 @@ NEGATIVE
 q3PosPattern1_1 = "(the force|force)"
 
 # pattern per frasi con negazione primo slot
-q3NegPattern1_1 = "(is not|isn't|not) (the force|force)"
+q3NegPattern1_1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (the force|force)"
 
 regQuestion3_1 = {"force": tuple([({q3PosPattern1_1,}),
                                   ({q3NegPattern1_1,})])}
 # pattern per frasi positive secondo slot
-q3PosPattern1_2 = "knowledge"
+q3PosPattern1_2 = "(knowledge|the knowledge)"
 
 # pattern per frasi con negazione secondo slot
-q3NegPattern1_2 = "(is not|isn't|not) knowledge"
+q3NegPattern1_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (knowledge|the knowledge)"
 
 regQuestion3_2 = {"knowledge": tuple([({q3PosPattern1_2}),
                                   ({q3NegPattern1_2})])}
 # pattern per frasi positive terzo slot
-q3PosPattern1_3 = "(self discipline|self-discipline)"
+q3PosPattern1_3 = "(self discipline|self-discipline|the self discipline|the self-discipline)"
 
 # pattern per frasi con negazione terzo slot
-q3NegPattern1_3 = "(is not|isn't|not) (self discipline|self-discipline)"
+q3NegPattern1_3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (self discipline|self-discipline|the self discipline|the self-discipline)"
 
 regQuestion3_3 = {"self discipline": tuple([({q3PosPattern1_3}),
                                   ({q3NegPattern1_3})])}
@@ -207,8 +207,8 @@ q4PosPattern3 = "powered by a kiber crystal"
 # pattern per frasi con negazione
 q4NegPattern1 = "^((?!kyber).)*$"
 q4NegPattern2 = "((?!kyber).)*$ cristal"
-q4NegPattern3 = "(is not|not|isn't) kiber cristal"
-q4NegPattern4 = "(is not|not|isn't) (powered|power)"
+q4NegPattern3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) kiber cristal"
+q4NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (powered|power)"
 
 regQuestion4 = {"kyber": tuple([({q4PosPattern1, q4PosPattern2, q4PosPattern3}),
                                 ({q4NegPattern1, q4NegPattern2, q4NegPattern3, q4NegPattern4})])}
@@ -240,10 +240,10 @@ q5PosPattern5 = "it is order (66|sixty-six|sixty six)"
 # pattern per frasi con negazione
 q5NegPattern1 = "^((?!(66|sixty-six|sixty six)).)*$"
 q5NegPattern2 = "order ((?!(66|sixty-six|sixty six)).)*$"
-q5NegPattern3 = "(is not|not|isn't) (66|sixty-six|sixty six)"
-q5NegPattern4 = "(is not|not|isn't) order (66|sixty-six|sixty six)"
-q5NegPattern5 = "(is not|not|isn't) to (gave|issue) order (66|sixty-six|sixty six)"
-q5NegPattern6 = "(don't|do not) (think|belive|suppose|assume|repute|opine) it's order (66|sixty-six|sixty six)"
+q5NegPattern3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (66|sixty-six|sixty six)"
+q5NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) order (66|sixty-six|sixty six)"
+q5NegPattern5 = "(is not|isn't|not|it's|didn't|did not|don't|do not) to (gave|issue) order (66|sixty-six|sixty six)"
+q5NegPattern6 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (think|belive|suppose|assume|repute|opine) it's order (66|sixty-six|sixty six)"
 
 regQuestion5 = {"order": tuple([({q5PosPattern1, q5PosPattern2, q5PosPattern3, q5PosPattern4, q5PosPattern5}),
                                 ({q5NegPattern1, q5NegPattern2, q5NegPattern3, q5NegPattern4, q5NegPattern5,
@@ -269,7 +269,7 @@ q6PosPattern1 = "yoda"
 q6NegPattern1 = "is ((?!yoda).)*$"
 q6NegPattern2 = "^((?!yoda).)*$"
 q6NegPattern3 = "grand master of the jedi council is ((?!yoda).)*$"  # se formula la classica risposta ma con un altro nome
-q6NegPattern4 = "(is not|not|isn't) yoda"
+q6NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) yoda"
 
 regQuestion6 = {"yoda": tuple([({q6PosPattern1}),
                                ({q6NegPattern1, q6NegPattern2, q6NegPattern3, q6NegPattern4})])}
@@ -295,7 +295,7 @@ q7PosPattern2 = "is red"
 # pattern per frasi con negazioneSW
 q7NegPattern1 = "is ((?!red).)*$"
 q7NegPattern2 = "^((?!red).)*$"
-q7NegPattern3 = "(is not|not|isn't) red"
+q7NegPattern3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) red"
 q7NegPattern4 = "(don't have|have no) color"
 
 regQuestion7 = {"color": tuple([({q7PosPattern1, q7PosPattern2}),
@@ -322,7 +322,7 @@ q9PosPattern1 = "dagobah"
 q9NegPattern1 = "is ((?!dagobah).)*$"
 q9NegPattern2 = "^((?!dagobah).)*$"
 q9NegPattern3 = "the planet where he hid is ((?!yoda).)*$"
-q9NegPattern4 = "(is not|not|isn't) dagobah"
+q9NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) dagobah"
 
 regQuestion9 = {"dagobah": tuple([({q9PosPattern1}),
                                   ({q9NegPattern1, q9NegPattern2, q9NegPattern3, q9NegPattern4})])}
@@ -351,7 +351,7 @@ q8PosPattern3 = "role of (master|teacher)"
 q8NegPattern1 = "^((?!master).)*$"
 q8NegPattern2 = "is ((?!master).)*$"
 q8NegPattern3 = "role of ((?!master).)*$"
-q8NegPattern4 = "(is not|not|isn't) dagobah"
+q8NegPattern4 = "(is not|isn't|not|it's|didn't|did not|don't|do not) dagobah"
 
 regQuestion8 = {"master": tuple([({q8PosPattern1, q8PosPattern2, q8PosPattern3}),
                                  ({q8NegPattern1, q8NegPattern2, q8NegPattern3, q8NegPattern4})])}
@@ -375,7 +375,7 @@ he was granted/allowed
 """
 q10PosPattern1 = "no"
 q10PosPattern2 = "(has|have|was) never (received|granted|recognized|allowed)"
-q10PosPattern3 = "(not|didn't|did not) (received|granted|recognized|allowed)"
+q10PosPattern3 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (received|granted|recognized|allowed)"
 
 # pattern per frasi con negazioneSW
 q10NegPattern1 = "yes"
@@ -417,26 +417,91 @@ q11PosPattern2_1 = "role of (general|generals)"
 q11PosPattern3_1 = "the (general|generals)"
 q11PosPattern4_1 = "can be (general|generals)"
 
-q10NegPattern1_1 = "^((?!(general|commander)).)*$"
-q10NegPattern2_1 = "(not|didn't|did not) (general|generals)"
-q10NegPattern3_1 = "(not|didn't|did not) the (position|role) of (general|generals)"
-q10NegPattern4_1 = "(can't|can not) be (general|generals)"
+q11NegPattern1_1 = "^((?!(general|generals)).)*$"
+q11NegPattern2_1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (general|generals)"
+q11NegPattern3_1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the (position|role) of (general|generals)"
+q11NegPattern4_1 = "(can't|can not) be (general|generals)"
 
-regQuestion11_1 = {"general": tuple([({}),
-                                  ({})])}
+regQuestion11_1 = {"general": tuple([({q11PosPattern1_1,q11PosPattern2_1,q11PosPattern3_1,q11PosPattern4_1}),
+                                  ({q11NegPattern1_1,q11NegPattern2_1,q11NegPattern3_1,q11NegPattern4_1})])}
 
 q11PosPattern1_2 = "(commander|commanders)"
 q11PosPattern2_2 = "role of (commander|commanders)"
 q11PosPattern3_2 = "the (commander|commanders)"
 q11PosPattern4_2 = "can be (commander|commanders)"
 
-q10NegPattern1_2 = "^((?!(general|commander|generals|commanders)).)*$"
-q10NegPattern2_2 = "(not|didn't|did not) (commander|commanders)"
-q10NegPattern3_2 = "(not|didn't|did not) the (position|role) of (commander|commanders)"
-q10NegPattern4_2 = "(can't|can not) be (commander|commanders)"
+q11NegPattern1_2 = "^((?!(commander|commanders)).)*$"
+q11NegPattern2_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) (commander|commanders)"
+q11NegPattern3_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the (position|role) of (commander|commanders)"
+q11NegPattern4_2 = "(can't|can not) be (commander|commanders)"
 
-regQuestion11_1 = {"Commander": tuple([({}),
-                                  ({})])}
+regQuestion11_2 = {"commander": tuple([({q11PosPattern1_2,q11PosPattern2_2,q11PosPattern3_2,q11PosPattern4_2}),
+                                  ({q11NegPattern1_2,q11NegPattern2_2,q11NegPattern3_2,q11NegPattern4_2})])}
+
+# Domanda: what are the three jedi orders ? -------------------------------------------------------------------------------------------------------------------
+"""
+POSITIVE slot 1
+--------------------------------
+guardian
+the guardians
+the order of the guardians
+
+NEGATIVE slot 1
+--------------------------------
+not the guardian
+not the order of the guardians
+
+POSITIVE slot 2
+--------------------------------
+sentinel
+the sentinels
+the order of the sentinels
+
+NEGATIVE slot 2
+--------------------------------
+not the order of the sentinels
+
+
+POSITIVE slot 3
+--------------------------------
+consular
+the consular
+the order of the consular
+
+NEGATIVE slot 3
+--------------------------------
+not the order of the consular
+
+"""
+q12PosPattern1_1 = "(guardian|guardians)"
+q12PosPattern2_1 = "the (guardian|guardians)"
+q12PosPattern3_1 = "the order of the (guardian|guardians)"
+
+q12NegPattern1_1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the order of the (guardian|guardians)"
+q12NegPattern2_1 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the (guardian|guardians)"
+
+regQuestion12_1 = {"guardian": tuple([({q12PosPattern1_1,q12PosPattern2_1,q12PosPattern3_1}),
+                                  ({q12NegPattern1_1,q12NegPattern2_1})])}
+
+q12PosPattern1_2 = "(sentinel|sentinels)"
+q12PosPattern2_2 = "the (sentinel|sentinels)"
+q12PosPattern3_2 = "the order of the (sentinel|sentinels)"
+
+q12NegPattern1_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the order of the (sentinel|sentinels)"
+q12NegPattern2_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the (sentinel|sentinels)"
+
+regQuestion12_2 = {"sentinel": tuple([({q12PosPattern1_2,q12PosPattern2_2,q12PosPattern3_2}),
+                                  ({q12NegPattern1_2,q12NegPattern2_2})])}
+
+q12PosPattern1_3 = "consular"
+q12PosPattern2_3 = "the consular"
+q12PosPattern3_3 = "the order of the consular"
+
+q12NegPattern1_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the order of the consular"
+q12NegPattern2_2 = "(is not|isn't|not|it's|didn't|did not|don't|do not) the consular"
+
+regQuestion12_3 = {"consular": tuple([({q12PosPattern1_3,q12PosPattern2_3,q12PosPattern3_3}),
+                                  ({q12NegPattern1_2,q12NegPattern2_2})])}
 
 # testiamo se il pattern crea un match
 #match = re.search(q5NegPattern5, "I don't think it's order 66")
