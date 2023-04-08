ca74f641e833011a9db5aa3bd07421e72371a02f
Niciolla <39137998+Niciolla@users.noreply.github.com>
2023-04-08 17:58:43 +0200

    Speech bugfix: numbers as seq of digits were not pronounced by the speech synthesis model

--- speech/SpeechSynthesizer.py
+++ speech/SpeechSynthesizer.py
@@ -1,6 +1,8 @@
 import os
 import io
 import contextlib
+import re
+
 import pydub.playback
 from TTS.api import TTS
 from threading import Thread
@@ -22,10 +24,21 @@ class SpeechSynthesizer:
         print("\nDone.\n")
 
     def play(self, text_to_reproduce):
+        digit_dict = {0:"zero",1: "one",2: "two",3:"three",
+                      4:"four",5:"five",6:"six",7:"seven",
+                      8: "eight",9 :"nine", 10: "ten", 11:"eleven", 12:"twelve"}
+        new_text_to_reproduce = ""
+        for word in text_to_reproduce.split(" "):
+            if word == "Obi-1":
+                new_text_to_reproduce += "Obi one "
+            elif word.isnumeric() and int(word) in digit_dict:
+                new_text_to_reproduce += f"{digit_dict[int(word)]} "
+            else:
+                new_text_to_reproduce += word + " "
         self._clean_dir()
         try:
             kwargs = {
-                "text": text_to_reproduce,
+                "text": new_text_to_reproduce,
                 "speaker_wav": "speech/wav_dir/obi-wan-kenobi.wav",
                 "language": "en",
                 "file_path": self.file_path}
