b6f37eb5cb455fde3195436bef6c8db1b37dc8b0
Niciolla <39137998+Niciolla@users.noreply.github.com>
2023-04-08 12:51:25 +0200

    speech recognition is working correctly.

--- speech/SpeechRecognitionHandler.py
+++ speech/SpeechRecognitionHandler.py
@@ -1,18 +1,27 @@
 import speech_recognition
 
+class SpeechException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 class SpeechRecognitionHandler:
     def __init__(self):
         self.handler = speech_recognition.Recognizer()
 
     def speak(self):
-        with speech_recognition.Microphone() as audio_src:
-            print("\nI'm listening...")
-            audio = self.handler.listen(audio_src)
-        try:
-            print(self.handler.recognize_google(audio))
-        except speech_recognition.UnknownValueError:
-            print("Quite not catched that. Can you repeat?")
-        except speech_recognition.RequestError:
-            print("ERROR: Speech Recognition API is not currently working")
+        can_continue = True
+        while can_continue:
+            with speech_recognition.Microphone() as audio_src:
+                print("\nI'm listening...")
+                audio = self.handler.listen(audio_src)
+            try:
+                converted_text = self.handler.recognize_google(audio)
+                return converted_text
+            except speech_recognition.UnknownValueError:
+                print("Quite not catched that. Can you repeat?")
+            except speech_recognition.RequestError:
+                raise SpeechException("ERROR: Speech Recognition API is not currently working")
+
+
 
